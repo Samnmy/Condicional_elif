50b2faa4f37631e4c02cd9cdd2c3b91fa3f8ee84
Sammy <samuel.monsalve.orrego@gmail.com>
2025-05-05 12:05:42 -0500

    Ejercicio_1.py

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,6 +1,6 @@
-N1=float(input("Dame : "))
-N2=float(input("Dame el primer numero: "))
-N3=float(input("Dame el primer numero: "))
+N1=float(input("Dame la primero nota : "))
+N2=float(input("Dame la segunda nota : "))
+N3=float(input("Dame la tercer nota : "))
 P=(N1+N2+N3)/3
 if P >= 7:
     print("Promocionado")
